a1d1b8939397a5262f83daf61e989f0a07439644
Ning Sheng Ying <nsynsy.dev@gmail.com>
2026-04-08 15:02:59 +0800

    feat: regex constants for string validation

---
+++ string/regexes.ts
@@ -0,0 +1,3 @@
+export const REGEX_NO_SPECIAL_CHARS = /^[a-zA-Z0-9-_.]*$/;
+export const REGEX_START_END_WITH_ALPHANUMERIC = /^[a-zA-Z0-9](?:.*[a-zA-Z0-9])?$/;
+export const REGEX_NO_CONSECUTIVE_DELIMITERS = /^(?!.*[._-]{2})[a-zA-Z0-9._-]+$/;
